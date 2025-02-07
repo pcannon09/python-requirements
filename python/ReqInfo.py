35fa5f20af4f0ee873f606d5479bc66c29b8d4cb
pcannon09 <pcannon775@gmail.com>
2025-02-07 16:37:51 +0000

    Set the latest version (1.0.0)

--- python/ReqInfo.py
+++ python/ReqInfo.py
@@ -1,10 +1,10 @@
 class ReqInfo:
-    VERSION_MAJOR: int = 0
+    VERSION_MAJOR: int = 1
     VERSION_MINOR: int = 0
     VERSION_PATCH: int = 0
 
-    VERSION_STATE: str = "dev"
+    VERSION_STATE: str = "public"
 
     VERSION_TOTAL: str = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}"
 
-    DEV: bool = True
+    DEV: bool = False
