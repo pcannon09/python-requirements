0d66ffdb7d0587ae209d9fd49028f864900db6f6
pcannon09 <pcannon775@gmail.com>
2025-02-07 16:21:56 +0000

    Build and run the project anywhere!

---
+++ install.sh
@@ -0,0 +1,31 @@
+# NOTE: Make sure to have `pyinstaller` installed
+
+if [[ "$1" == "" ]]; then
+    compileFiles=(
+        "main.py"
+        "File.py"
+        "Debug.py"
+        "ReqInfo.py"
+    )
+
+    compileFilesStr=""
+
+    for compileFilesIt in "${compileFiles[@]}"; do
+        compileFilesStr+="${compileFilesIt} "
+    done
+
+    echo "Files to compile: $compileFilesStr"
+
+    rm -rf ./python/build
+    rm -rf ./python/dist
+
+    cd python
+
+    pyinstaller --onefile $compileFilesStr
+
+    mv ./python/dist/main ./python/dist/py-req
+
+elif [[ "$1" == "run" ]]; then
+    ./python/dist/main/py-req "$2"
+fi
+
