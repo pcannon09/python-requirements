f682468cb5f995005ef24acd246deae93fe4c20b
pcannon09 <pcannon775@gmail.com>
2025-02-08 15:42:59 +0100

    Update to version 1.1.0

--- python/ReqInfo.py
+++ python/ReqInfo.py
@@ -1,10 +1,4 @@
 class ReqInfo:
-    VERSION_MAJOR: int = 1
-    VERSION_MINOR: int = 0
-    VERSION_PATCH: int = 0
-
-    VERSION_STATE: str = "public"
-
-    VERSION_TOTAL: str = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}"
+    VERSION_MAJOR: tuple = (1, 1, 0, "public")
 
     DEV: bool = False
