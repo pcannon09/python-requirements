8b614b59422022b083d38d34ad4bf44f59b6fe7a
pcannon09 <pcannon775@gmail.com>
2025-03-04 20:39:38 +0100

    Added option to call shell directly from command line and call commands in the shell [ BETA ]

--- python/execute.py
+++ python/execute.py
@@ -3,6 +3,17 @@ from Debug import debug
 from ReqInfo import ReqInfo
 
 import sys
+import readline
+
+def outMsg(file: File, out: list):
+    if (out[0] == 0):
+        debug.debug(f"Done parsing `{file.id}`...", "info")
+
+    elif (out[0] < 0):
+        debug.debug(f"Failed to parse `{file.id}`...", "error")
+
+    elif (out[0] == 1):
+        debug.debug(f"Warning when parsing `{file.id}`...", "warn")
 
 def main():
     filePath: str = ".requirements"
@@ -21,7 +32,7 @@ def main():
 
             sys.exit(0)
 
-        elif (sys.argv[1] == "--path"):
+        elif (sys.argv[1] == "--path" or sys.argv[1] == "-p"):
             if (len(sys.argv) > 2):
                 filePath = sys.argv[2]
 
@@ -29,6 +40,83 @@ def main():
                 print("Parameter `path` was not provided. Please provide a valid path")
 
                 sys.exit(1)
+
+        elif (sys.argv[1] == "--shell" or sys.argv[1] == "-s"):
+            line: int = 1
+            totalCommand: list = [[line, ""]]
+            totalCommandStr: str = ""
+
+            if (len(sys.argv) > 2):
+                if (sys.argv[2] != ""):
+                    file: File = File("shell-parse-req-main-param", "")
+                    out = file.parse(totalCommandStr)
+
+                    print(f"Exit code:    {out[0]}")
+                    print(f"Exit message: {out[1]}")
+
+                    outMsg(file, out)
+
+                    sys.exit(0)
+
+            while (1):
+                command: str = input(f"{line} >>> ")
+
+                if (command == "EXIT"):
+                    break
+
+                elif (command.lower() == "HELP".lower()):
+                    print("* HELP")
+                    print("Commands:")
+                    print("[ HELP ] Show this help")
+                    print("[ EXIT ] Exit shell")
+                    print("[ RUN ] Run the previous scripts")
+                    print("[ RESET ] Clear all the commands and previous scripts")
+
+                    continue
+
+                elif (command == "RUN"):
+                    if (len(totalCommandStr) == 0):
+                        print("Cannot RUN shell. Make sure you have a script that contains something")
+
+                        continue
+
+                    file: File = File("shell-parse-req-main", "")
+                    out = file.parse(totalCommandStr)
+
+                    print(f"Exit code:    {out[0]}")
+                    print(f"Exit message: {out[1]}")
+
+                    outMsg(file, out)
+
+                    continue
+                
+                elif (command == "RESET"):
+                    line = 0
+                    totalCommandStr = ""
+                    totalCommand = [0, ""]
+
+                    continue
+
+                elif (command.split(" ")[0] == "GOTO"):
+                    print(f"GOTO L{command.split(" ")[1]}")
+
+                    try:
+                        del totalCommand[int(command.split(" ")[1]):]
+
+                    except ValueError:
+                        print("[ ERROR ] Please provide an int")
+
+                        continue
+
+                    totalCommandStr = totalCommand[int(command.split(" ")[1])]
+
+                    continue
+
+                line += 1
+                totalCommandStr += f"{command}\n"
+                totalCommand.append([line, f"{command}\n"])
+
+            sys.exit(0)
 
     debug.debug(f"Opening `{filePath}`...", "info")
 
@@ -41,12 +129,5 @@ def main():
     print(f"Exit code:    {out[0]}")
     print(f"Exit message: {out[1]}")
 
-    if (out[0] == 0):
-        debug.debug(f"Done parsing `{file.id}`...", "info")
-
-    elif (out[0] < 0):
-        debug.debug(f"Failed to parse `{file.id}`...", "error")
-
-    elif (out[0] == 1):
-        debug.debug(f"Warning when parsing `{file.id}`...", "warn")
+    outMsg(file, out)
 
