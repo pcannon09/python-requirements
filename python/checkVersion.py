f316317b736c6cb593e54c2d8ce1d3afccec5ee0
pcannon09 <pcannon775@gmail.com>
2025-03-26 06:40:41 +0000

    Minor changes

--- python/checkVersion.py
+++ python/checkVersion.py
@@ -1,6 +1,11 @@
 import sys
 
 def getPythonVersion():
+    """
+    @brief Get the current python version
+    This can be used to check if the current python version is available
+    """
+
     if (sys.version_info < (3, 6)):
         print("[ ERR ] This code requires at least Python 3.6")
 
